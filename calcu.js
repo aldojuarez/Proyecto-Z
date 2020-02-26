bf3597fb5882fb9bf193684831875570402a3d56
yanitza guadalupe mungaro monge <yanitza_guadalupe@hotmail.com>
2020-02-26 10:12:43 -0700

    mejora de resultados

--- calcu.js
+++ calcu.js
@@ -8,48 +8,11 @@ function calculos(valor){
     }
     
 }
-
-function resultado(){
-    var pant=document.getElementById("pantalla").value;
-    var suma = pant.indexOf("+");
-    var resta = pant.indexOf("-");
-    var multi = pant.indexOf("*");
-    var division = pant.indexOf("/");
-    var residuo = pant.indexOf("%");
-    var raiz = pant.indexOf("√");
-    if(suma!=-1){
-        var primero= pant.split("+");
-        var resultado= parseInt(primero[0])+parseInt(primero[1]);
-        limpia();
-        document.getElementById("pantalla").value=resultado;
-    }else if(resta!=-1){
-        var primero= pant.split("-");
-        var resultado= parseInt(primero[0])-parseInt(primero[1]);
-        limpia();
-        document.getElementById("pantalla").value=resultado;
-    }else if(multi!=-1){
-        var primero= pant.split("*");
-        var resultado= parseInt(primero[0])*parseInt(primero[1]);
-        limpia();
-        document.getElementById("pantalla").value=resultado;
-    } else if(division!=-1){
-        var primero= pant.split("/");
-        var resultado= parseInt(primero[0])/parseInt(primero[1]);
-        limpia();
-        document.getElementById("pantalla").value=parseFloat(resultado);
-    }else if(residuo!=-1){
-        var primero= pant.split("%");
-        var resultado= parseInt(primero[0])%100;
-        limpia();
-        document.getElementById("pantalla").value=resultado;
-    }else if(raiz!=-1){
-        var primero= pant.split("√");
-        var resultado=Math.sqrt(parseFloat(primero[1]));
-        limpia();
-        document.getElementById("pantalla").value=parseFloat(resultado);
-    }     
-}
 function limpia(){
     document.getElementById("pantalla").value="";
+}
+
+function resultado (){
+    document.getElementById("pantalla").value= eval(document.getElementById("pantalla").value);
 }
     
